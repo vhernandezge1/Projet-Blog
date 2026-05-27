7ad39bb1e559d751c8255c2b54477dc7e481c945
Valentin <valentinhernandez420@yahoo.fr>
2026-05-27 17:51:50 +0200

    scss files

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,16 @@
 import { defineConfig } from 'vite';
+import { fileURLToPath } from 'node:url';
 
 export default defineConfig({
-  root: './src',
+  root: fileURLToPath(new URL('./src', import.meta.url)),
   build: {
-    outDir: '../dist',
+    outDir: fileURLToPath(new URL('./dist', import.meta.url)),
     emptyOutDir: true,
+    rolldownOptions: {
+      input: {
+        index: fileURLToPath(new URL('./src/index.html', import.meta.url)),
+        form: fileURLToPath(new URL('./src/form/form.html', import.meta.url)),
+      },
+    },
   },
 });
